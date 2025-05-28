5507e8406ab39d892fdad176dec0f718cde57d3a
unknown <muamasy159@gmail.com>
2025-05-28 14:56:15 +0800

    Arrow top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,5 +32,25 @@
                 {{ $slot }}
             </main>
         </div>
+
+        <!-- Scroll to Top Button -->
+        <div x-data="{ show: false }" 
+             x-init="
+                show = window.pageYOffset > 100;
+                window.addEventListener('scroll', () => show = window.pageYOffset > 100)
+             "
+             x-show="show"
+             x-transition:enter="transition ease-out duration-150"
+             x-transition:enter-start="opacity-0 transform translate-y-2"
+             x-transition:enter-end="opacity-100 transform translate-y-0"
+             x-transition:leave="transition ease-in duration-150"
+             x-transition:leave-start="opacity-100 transform translate-y-0"
+             x-transition:leave-end="opacity-0 transform translate-y-2"
+             @click="window.scrollTo({top: 0, behavior: 'smooth'})"
+             class="fixed bottom-8 right-8 bg-indigo-600 text-white p-3 rounded-full shadow-lg hover:bg-indigo-700 cursor-pointer transition-all duration-150 hover:scale-110 z-50">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18" />
+            </svg>
+        </div>
     </body>
 </html>
